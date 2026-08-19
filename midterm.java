5e191fe3cb2837dd6ae4535d06120c97d4d3af24
Matthew Macaraeg <100880838+Asheborne96@users.noreply.github.com>
2026-08-20 03:15:21 +0800

    Add midterm.java with main method and variables

---
+++ midterm.java
@@ -0,0 +1,22 @@
+public class midterm {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = 3;
+        int intResult = 0;
+        boolean x = true;
+        boolean y = false;
+        boolean booleanResult = false;
+
+        // intResult= a>>2;  // 2 >> 2 = 0
+        //booleanResult = x && !y; // true && true = true
+        // intResult = a | b + c;
+        //intResult = a & b;
+        //intResult = b << c ;
+        //  intResult +=c;
+         intResult = 1;
+
+        System.out.println("intResult = " + intResult);
+        System.out.println("booleanResult = " + booleanResult);
+    }
+}
